e8f73928e467a46e3ea6ce6bc42e45182af51df1
luohaonan <0376lhn@gmail.com>
2022-05-18 21:47:44 +0800

    giao

--- covid-19/zhb_modify_result.js
+++ covid-19/zhb_modify_result.js
@@ -1,54 +1,31 @@
-// match check-report.z.digitalcnzz.com:5443\/hs-check-result\/check\/result\/queryWithToken
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('e f(a,c){2 l=c.4;2 b,5,6,7,d=0,1;1=g h(j.k(a.4/3));b=1.4;m(2 i=0;i<b;i++){5=c.8(a.9(d));d++;6=c.8(a.9(d));d++;7=c.8(a.9(d));d++;1[i]=5*l*l+6*l+7}b=n("o.p("+1.q(\',\')+")");r b}',28,28,'|s|var||length|bl|b2|b3|indexOf|charAt|||||function|fromCode|new|Array||Math|floor||for|eval|String|fromCharCode|join|return'.split('|'),0,{}));
+;eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('e.f.g=h(a){6 o={"i+":0.7()+1,"d+":0.j(),"l+":0.n(),"m+":0.p(),"s+":0.r(),"q+":t.u((0.7()+3)/3),"v":0.w()};8(/(y+)/.9(a))a=a.b(2.$1,(0.x()+"").c(4-2.$1.5));z(6 k A o)8(B 2("("+k+")").9(a))a=a.b(2.$1,(2.$1.5==1)?(o[k]):(("C"+o[k]).c((""+o[k]).5)));D a}',40,40,'this||RegExp|||length|var|getMonth|if|test||replace|substr||Date|prototype|Format|function|M|getDate||H||getHours||getMinutes||getSeconds||Math|floor|S|getMilliseconds|getFullYear||for|in|new|00|return'.split('|'),0,{}));
 
-var default_value = {
-    "code": 0,
+let today = new Date().Format("yyyy-MM-dd")
+let yesterday = new Date(new Date().setDate(new Date().getDate()-1)).Format("yyyy-MM-dd")
+let name = fromCode("P5J0l6KTC","0l23456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+let o = {
     "message": "succ",
     "data": {
         "resultList": [
             {
-                "id": 4133335,
-                "idCard": "xxxxxxxxxxxxxxxxxx",
+                "idCard": "4xxxxxxxxxxxxxxx3",
+                "createTime": `${yesterday} 14:56:06`,
+                "id": 43311006,
                 "checkResult": "阴性",
-                "checkOrgName": "广州华银医学检验中心有限公司",
-                "reportTime": "2021-08-02 21:38:16",
-                "createTime": "2021-08-04 02:19:36"
+                "activyId": 202205,
+                "checkOrgName": "郑州和合医学检验实验室",
+                "name": name,
+                "reportTime": `${today} 00:45:08`
             }
         ]
-    }
+    },
+    "code": 0
 }
 
+// 罗*楠
+let body = JSON.stringify(o)
 
-function padding(no) {
-    if (no < 10) {
-        return "0" + no
-    }
-    return no
-}
+$done({ body });
 
-
-function getYesterday(date) {
-    let yesterday = new Date(new Date() - 1000 * 60 * 60 * 24)
-    return `${yesterday.getFullYear()}-${padding(yesterday.getMonth() + 1)}-${padding(yesterday.getDate())}${date.substring(10, 22)}`
-}
-
-
-
-function Change_ResponseBody() {
-    let item = default_value
-    let obj = null
-    if ($response.status == 200) {
-        obj = JSON.parse($response.body)
-        if (obj.code == 0 && obj.data.resultList.length >= 1) {
-            // obj.data.resultList = Array(obj.data.resultList.shift())
-            item = obj
-        }
-    }
-    item.data.resultList[0].reportTime = getYesterday(item.data.resultList[0].reportTime)
-    item.data.resultList[0].createTime = getYesterday(item.data.resultList[0].createTime)
-    item.data.resultList[0].checkResult = "阴性"
-    let body = JSON.stringify(item)
-    $done({ body });
-
-}
-
-Change_ResponseBody()
---
+++ covid-19/zhb_modify_result_copy.js
@@ -0,0 +1,54 @@
+// match check-report.z.digitalcnzz.com:5443\/hs-check-result\/check\/result\/queryWithToken
+
+var default_value = {
+    "code": 0,
+    "message": "succ",
+    "data": {
+        "resultList": [
+            {
+                "id": 4133335,
+                "idCard": "xxxxxxxxxxxxxxxxxx",
+                "checkResult": "阴性",
+                "checkOrgName": "广州华银医学检验中心有限公司",
+                "reportTime": "2021-08-02 21:38:16",
+                "createTime": "2021-08-04 02:19:36"
+            }
+        ]
+    }
+}
+
+
+function padding(no) {
+    if (no < 10) {
+        return "0" + no
+    }
+    return no
+}
+
+
+function getYesterday(date) {
+    let yesterday = new Date(new Date() - 1000 * 60 * 60 * 24)
+    return `${yesterday.getFullYear()}-${padding(yesterday.getMonth() + 1)}-${padding(yesterday.getDate())}${date.substring(10, 22)}`
+}
+
+
+
+function Change_ResponseBody() {
+    let item = default_value
+    let obj = null
+    if ($response.status == 200) {
+        obj = JSON.parse($response.body)
+        if (obj.code == 0 && obj.data.resultList.length >= 1) {
+            // obj.data.resultList = Array(obj.data.resultList.shift())
+            item = obj
+        }
+    }
+    item.data.resultList[0].reportTime = getYesterday(item.data.resultList[0].reportTime)
+    item.data.resultList[0].createTime = getYesterday(item.data.resultList[0].createTime)
+    item.data.resultList[0].checkResult = "阴性"
+    let body = JSON.stringify(item)
+    $done({ body });
+
+}
+
+Change_ResponseBody()
